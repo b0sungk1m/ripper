3c5f6e3dd8361404edcd3e415478fa387cb478d4
b0sungk1m <bosungkim1712@gmail.com>
2025-05-15 14:03:27 -0400

    only add dashboard.

--- src/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Ripper's Trading System
-""" 
--- src/strategies/custom/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-🌙 Moon Dev's Custom Strategies Package
-"""
-from src.strategies.base_strategy import BaseStrategy
-from .example_strategy import ExampleStrategy
-from .macd_cross_strategy import MACDStrategy
-
-__all__ = ['ExampleStrategy', 'MyStrategy', 'MACDStrategy'] 
